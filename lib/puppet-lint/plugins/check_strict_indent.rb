7dc5d2cf61c64c0280a994473c28c02d2495a395
Mike Salway <38225053+msalway@users.noreply.github.com>
2025-08-02 12:33:38 +0100

    Refactor heredoc indent calculations

--- lib/puppet-lint/plugins/check_strict_indent.rb
+++ lib/puppet-lint/plugins/check_strict_indent.rb
@@ -7,22 +7,19 @@ PuppetLint.new_check(:strict_indent) do
       RBRACE: :LBRACE,
       RBRACK: :LBRACK,
       RPAREN: :LPAREN,
-      HEREDOC: :HEREDOC_OPEN,
-      HEREDOC_POST: :HEREDOC_OPEN,
     }
     open = {
       LBRACE: [],
       LBRACK: [],
       LPAREN: [],
-      HEREDOC_OPEN: [],
     }
 
     matches = {}
 
     tokens.each do |token|
-      if %i[LBRACE LBRACK LPAREN HEREDOC_OPEN].include?(token.type)
+      if %i[LBRACE LBRACK LPAREN].include?(token.type)
         open[token.type] << token
-      elsif %i[RBRACE RBRACK RPAREN HEREDOC HEREDOC_POST].include?(token.type)
+      elsif %i[RBRACE RBRACK RPAREN].include?(token.type)
         match = open[opening_token[token.type]].pop
         unless match.nil?
           matches[token] = match
@@ -132,13 +129,27 @@ PuppetLint.new_check(:strict_indent) do
       end
 
       # get actual indent
-      actual = 0
-      actual = if token.next_token.type == :INDENT
+      actual = case token.next_token.type
+               when :INDENT
                  token.next_token.value.length
-               elsif !token.prev_token.nil? and token.prev_token.type == :HEREDOC
-                 token.prev_token.value.split("\n").last.length
-               elsif !token.prev_token.nil? and token.prev_token.type == :HEREDOC_OPEN
-                 next_token.prev_token.value.split("\n").last.length
+               when :HEREDOC
+                 # Lines containing heredocs have no indent token as the indent is consumed by the heredoc token.
+                 # However the last line of the token value is the whitespace before the pipe in the termination line.
+                 # We use the length of this to get the indent.
+                 token.next_token.value.split("\n").last.length
+               when :HEREDOC_PRE
+                 # For interpolated heredocs the pipe whitespace is in the HEREDOC_POST token so we need scan forward
+                 # to this and get its length.
+                 next_token = token.next_token
+                 while !next_token.nil? and next_token.type != :NEWLINE and next_token.type != :HEREDOC_POST
+                   next_token = next_token.next_token
+                 end
+                 if next_token.type == :HEREDOC_POST
+                   next_token.value.split("\n").last.length
+                 else
+                   # Should never get here return zero if we do
+                   0
+                 end
                else
                  0
                end
